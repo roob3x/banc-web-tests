6951faa3e44e7d9b2f7288b7fd20b095e3f0f5e8
Roberto Filho <roberto.siilva@hotmail.com>
2026-04-27 16:47:44 -0300

    added test to validate login wih invalid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -10,4 +10,17 @@ describe('Login', () => {
     // Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
+
+  it('Login com dados invalidos devem apresentar mensagem de erro', () => {
+    // Arrange
+    cy.visit('http://localhost:4000')
+    // Act
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('654321')
+    cy.contains('button', 'Entrar').click()
+    //cy.get('#login-section > .btn').click()
+
+    // Assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+  })
 })
