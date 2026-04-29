afd406a6ed1a46400b713d282eebea2b895dc7b1
Roberto Filho <roberto.siilva@hotmail.com>
2026-04-29 12:27:37 -0300

    added report using mockwesome

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,10 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:4000',
+    reporter: 'cypress-mochawesome-reporter',
     //video: true,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
   },
